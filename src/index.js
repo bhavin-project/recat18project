9a2d39c49205ad904e1c619b59c62ea26b92358c
bhavin-project <bhavin.project@gmail.com>
2024-01-03 15:04:11 +0530

    props changes

--- src/index.js
+++ src/index.js
@@ -5,32 +5,42 @@ import "./index.css";
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        imgsrc="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQpn3dq-u_XGknKnmy4o9bU0YQyrq6F-XYajgHADytkgm1KwV9EINjzxZIrVp8ZuHbjRusbmNu-wm2oWCHtmP18rGqNCltKlpFRI8rfJ3FN&usqp=CAE"
+        title="Legendary"
+        author="Stephanie Garber"
+        alt="Legendary"
+      />
+      <Book
+        imgsrc="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ0IMP3VfKeEUsZjbZtO_5PZdLAbnbBWHQOJzvCIo0OmMflqnssfB6mxyaI1kgq-ArjnJo07OOSiKYYevhjdk6ZVjfuAw3SZrlgCsPuqiU7hQ7CXNidSuUDEA&usqp=CAE"
+        title="Demon Copperhead"
+        author="Barbara Kingsolver"
+        alt="Demon Copperhead"
+      />
+      <Book
+        imgsrc="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQxKl12kQSdr09-m6aMzhrqm5lhMqyRZ_dajHh7JXvXaQ2Vxg-NqtgV1XOfzRu2KEu8KkQUMCfmBUQK2s2jWYMRsVYVWF01RzaPyQLiYdM&usqp=CAE"
+        title="NW"
+        author="Zadie Smith"
+        alt="NW"
+      />
     </section>
   );
 };
-const author = "S. Jaishankar";
-const title = "Why Bharat Matters";
-const imgsrc =
-  "https://images-eu.ssl-images-amazon.com/images/I/71Hqj4xb0XL._AC_UL600_SR600,400_.jpg";
-const alt = "Why Bharat Matters";
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <img src={imgsrc} alt={alt} />
+      <img src={props.imgsrc} alt={props.alt} />
       <h2
         style={{
           color: "#6185b8",
-          fontSize: "0.85rem",
+          fontSize: "1rem",
           marginBottom: "0.2rem",
         }}
       >
-        {title}
+        {props.title}
       </h2>
-      <h4 style={{ color: "#e0bf63", fontSize: "0.5rem", marginTop: "0.3rem" }}>
-        {author}
+      <h4 style={{ color: "#e0bf63", fontSize: "0.8rem", marginTop: "0.3rem" }}>
+        {props.author}
       </h4>
     </article>
   );
