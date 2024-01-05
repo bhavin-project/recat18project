1843560b6c7794133097718cd3c8b4141fe4d0fa
bhavin-project <bhavin.project@gmail.com>
2024-01-05 17:10:27 +0530

    event-2

--- src/index.js
+++ src/index.js
@@ -2,27 +2,53 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const books = [
+  {
+    imgsrc:
+      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQpn3dq-u_XGknKnmy4o9bU0YQyrq6F-XYajgHADytkgm1KwV9EINjzxZIrVp8ZuHbjRusbmNu-wm2oWCHtmP18rGqNCltKlpFRI8rfJ3FN&usqp=CAE",
+    title: "Legendary",
+    author: "Stephanie Garber",
+    alt: "Legendary",
+    id: 1,
+  },
+  {
+    imgsrc:
+      "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ0IMP3VfKeEUsZjbZtO_5PZdLAbnbBWHQOJzvCIo0OmMflqnssfB6mxyaI1kgq-ArjnJo07OOSiKYYevhjdk6ZVjfuAw3SZrlgCsPuqiU7hQ7CXNidSuUDEA&usqp=CAE",
+    title: "Demon Copperhead",
+    author: "Barbara Kingsolver",
+    alt: "Demon Copperhead",
+    id: 2,
+  },
+  {
+    imgsrc:
+      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQxKl12kQSdr09-m6aMzhrqm5lhMqyRZ_dajHh7JXvXaQ2Vxg-NqtgV1XOfzRu2KEu8KkQUMCfmBUQK2s2jWYMRsVYVWF01RzaPyQLiYdM&usqp=CAE",
+    title: "NW",
+    author: "Zadie Smith",
+    alt: "NW",
+    id: 3,
+  },
+];
+
 const BookList = () => {
+  const valueSome = "Displaying Content";
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
   return (
     <section className="booklist">
-      <Book
-        imgsrc="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQpn3dq-u_XGknKnmy4o9bU0YQyrq6F-XYajgHADytkgm1KwV9EINjzxZIrVp8ZuHbjRusbmNu-wm2oWCHtmP18rGqNCltKlpFRI8rfJ3FN&usqp=CAE"
-        title="Legendary"
-        author="Stephanie Garber"
-        alt="Legendary"
-      />
-      <Book
-        imgsrc="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ0IMP3VfKeEUsZjbZtO_5PZdLAbnbBWHQOJzvCIo0OmMflqnssfB6mxyaI1kgq-ArjnJo07OOSiKYYevhjdk6ZVjfuAw3SZrlgCsPuqiU7hQ7CXNidSuUDEA&usqp=CAE"
-        title="Demon Copperhead"
-        author="Barbara Kingsolver"
-        alt="Demon Copperhead"
-      />
-      <Book
-        imgsrc="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQxKl12kQSdr09-m6aMzhrqm5lhMqyRZ_dajHh7JXvXaQ2Vxg-NqtgV1XOfzRu2KEu8KkQUMCfmBUQK2s2jWYMRsVYVWF01RzaPyQLiYdM&usqp=CAE"
-        title="NW"
-        author="Zadie Smith"
-        alt="NW"
-      />
+      {books.map((book) => {
+        return (
+          <Book
+            imgsrc={book.imgsrc}
+            alt={book.alt}
+            title={book.title}
+            author={book.author}
+            keyvalue={book.id}
+            bookfun={getBook}
+          />
+        );
+      })}
     </section>
   );
 };
@@ -42,6 +68,14 @@ const Book = (props) => {
       <h4 style={{ color: "#e0bf63", fontSize: "0.8rem", marginTop: "0.3rem" }}>
         {props.author}
       </h4>
+      <button
+        type="button"
+        name="clickBtn"
+        onClick={() => props.bookfun(props.keyvalue)}
+        id="clickBtn"
+      >
+        Click me!!!
+      </button>
     </article>
   );
 };
@@ -71,4 +105,4 @@ const EventTesting = () => {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<EventTesting />);
+root.render(<BookList />);
