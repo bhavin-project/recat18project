f32c04e7d1c18771e463eab72571f811b09c1019
bhavin-project <bhavin.project@gmail.com>
2024-01-06 11:44:40 +0530

    static book website

--- src/books.js
+++ src/books.js
@@ -1,23 +1,25 @@
+import img1 from "./images/1.webp";
+import img2 from "./images/2.webp";
+import img3 from "./images/3.webp";
+import img4 from "./images/nature.jpeg";
+
 const books = [
   {
-    imgsrc:
-      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQpn3dq-u_XGknKnmy4o9bU0YQyrq6F-XYajgHADytkgm1KwV9EINjzxZIrVp8ZuHbjRusbmNu-wm2oWCHtmP18rGqNCltKlpFRI8rfJ3FN&usqp=CAE",
+    imgsrc: img1,
     title: "Legendary",
     author: "Stephanie Garber",
     alt: "Legendary",
     id: 1,
   },
   {
-    imgsrc:
-      "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ0IMP3VfKeEUsZjbZtO_5PZdLAbnbBWHQOJzvCIo0OmMflqnssfB6mxyaI1kgq-ArjnJo07OOSiKYYevhjdk6ZVjfuAw3SZrlgCsPuqiU7hQ7CXNidSuUDEA&usqp=CAE",
+    imgsrc: img2,
     title: "Demon Copperhead",
     author: "Barbara Kingsolver",
     alt: "Demon Copperhead",
     id: 2,
   },
   {
-    imgsrc:
-      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQxKl12kQSdr09-m6aMzhrqm5lhMqyRZ_dajHh7JXvXaQ2Vxg-NqtgV1XOfzRu2KEu8KkQUMCfmBUQK2s2jWYMRsVYVWF01RzaPyQLiYdM&usqp=CAE",
+    imgsrc: img3,
     title: "NW",
     author: "Zadie Smith",
     alt: "NW",
